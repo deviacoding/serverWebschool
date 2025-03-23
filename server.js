c87ef6a911435c9ececbf401eeffcbdcb7e06f12
elie sultan <neguedayinhara@gmail.com>
2025-03-23 19:57:56 +0200

    udpate

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const app = express()
 const port = 8000
 const data = require("./data.json")
 const mysql = require("mysql2/promise")
+const cors = require('cors')
 
-
+app.use(cors())
 app.use(express.json())
 
 app.get('/', async function(req, res) {
@@ -25,7 +26,7 @@ app.get('/usersMySql', async (req, res) => {
 	
 	  try {
 		const [results, fields] = await connection.query(
-		  'SELECT * FROM `etudiants` '
+		  'SELECT * FROM `users` '
 		);
 	  
 		console.log(results); // results contains rows returned by server
@@ -35,6 +36,37 @@ app.get('/usersMySql', async (req, res) => {
 		console.log(err);
 		res.status(400).json({err: err})
 	  }
+})
+
+app.post('/quotes', async (req, res) => {
+	console.log(req.body)
+	const name = req.body.name ;
+	const email = req.body.email;
+	const phone = req.body.phone;
+	const service = req.body.service;
+	const commmentaire = req.body.commentaire
+	// const { name, email , phone, service } = req.body    // version destructuring
+	const connection = await  mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password : '',
+		database: 'webschool',
+	  });
+
+
+	try {
+		const [results] = await connection.query(
+			'INSERT INTO quotes (name, email, phone, service, commentaire) VALUES ( ? , ? , ? , ? , ? )'
+			, 
+			[name, email, phone, service, commmentaire]
+		)
+		console.log("ici sera mon result", results)
+		res.status(201).json({ message: 'Quote Created successfuly', id:results.insertId })
+		
+	} catch (error) {
+		console.log(error)
+	}
+
 })
 
 app.listen(port, () => {
